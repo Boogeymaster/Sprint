ee013e15a176382a9bb13ff3cb16b61027a0dcd7
Re <qwe@mail.com>
2025-01-29 20:08:02 +0300

    L1-T2

---
+++ src/main/kotlin/lesson1/Less1_Test2.kt
@@ -0,0 +1,14 @@
+package org.example.lesson1
+
+fun main() {
+    val itemCount: Short = 75
+    val thanksForBuy: String = "Thank you for purchase!"
+    var personalCount: Short = 2000
+
+    println(itemCount)
+    println(thanksForBuy)
+//    println(personalCount)
+    personalCount = 1999
+    println(personalCount)
+
+}
