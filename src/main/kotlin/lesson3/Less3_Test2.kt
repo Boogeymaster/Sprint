4999f06b0f63e71f29b29573930c93e119f1597d
Re <qwe@mail.com>
2025-02-02 20:50:22 +0300

    KS-L3-T2-interpolation

---
+++ src/main/kotlin/lesson3/Less3_Test2.kt
@@ -0,0 +1,13 @@
+package org.example.lesson3
+
+fun main() {
+
+    val name = "Татьяна"
+    val surName = "Андреева"
+    val patronymic = "Сергеевна"
+    val marriedName = "Сидорова"
+    val age1 = 20
+    val age2 = 22
+    println("$surName $name $patronymic, $age1 лет")
+    println("$marriedName $name $patronymic, $age2 года")
+}
