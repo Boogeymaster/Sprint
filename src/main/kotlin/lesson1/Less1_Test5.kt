b56e34cd0199b3c4f59889029dd00727cca48001
Re <qwe@mail.com>
2025-02-02 18:17:04 +0300

    KS-L1-T5-use-format()-and-%

--- src/main/kotlin/lesson1/Less1_Test5.kt
+++ src/main/kotlin/lesson1/Less1_Test5.kt
@@ -1,18 +1,10 @@
 package org.example.lesson1
 
 fun main(){
-    val seconds: Short = 6480
-    val minute = seconds / 60
-    val partOfMinute = seconds - minute * 60
-    val hour = minute / 60
-    val partOfHour = minute - hour * 60
-    println("${twoDigits(hour)}:${twoDigits(partOfHour)}:${twoDigits(partOfMinute)}")
-}
-fun twoDigits(num: Int): String {
-    val twoDig = if (num < 10) {
-        "0$num"
-    } else {
-        "$num"
-    }
-    return twoDig
+    val secondsOfJourney: Short = 6480
+    val minutesOfJourney = secondsOfJourney / 60
+    val hoursOfJourney = minutesOfJourney / 60
+    println("${String.format("%02d", hoursOfJourney)}:${String.format("%02d", minutesOfJourney % 60)}:" +
+            String.format("%02d", secondsOfJourney % 60)
+    )
 }
