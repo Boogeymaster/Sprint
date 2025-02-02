3616771e8599d85796dd27ee6c3a9e5d08026927
Re <qwe@mail.com>
2025-02-02 21:31:03 +0300

    KS-L3-T4-chess

---
+++ src/main/kotlin/lesson3/Less3_Test4.kt
@@ -0,0 +1,14 @@
+package org.example.lesson3
+
+fun main() {
+
+    var from = "e2"
+    var where = "e4"
+    var numberOfMove = 1
+    println("$from-$where; $numberOfMove")
+    from = "d2"
+    where = "d3"
+    numberOfMove++
+    println("$from-$where; $numberOfMove")
+
+}
