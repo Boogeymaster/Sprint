a2878e4b47821128c7729b1615e097c62cb9a79e
Re <qwe@mail.com>
2025-02-02 17:35:19 +0300

    KS-L2-T1 use extension

--- src/main/kotlin/lesson2/Less2_Test1.kt
+++ src/main/kotlin/lesson2/Less2_Test1.kt
@@ -1,12 +1,6 @@
 package org.example.lesson2
 
 fun main() {
-    var sum = 0f
     val studentsPoint = listOf(3f, 4f, 3f, 5f)
-    studentsPoint.forEach {
-        sum += it
-    }
-    val averagePoint: Float = sum / studentsPoint.size
-    println(averagePoint)
-
+    println(studentsPoint.sum() / studentsPoint.size)
 }
