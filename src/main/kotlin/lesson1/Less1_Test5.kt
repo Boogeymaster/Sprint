7344a11902f072fdc6a6ee38adea086daa263ea5
Re <qwe@mail.com>
2025-02-03 10:28:23 +0300

    KS-1-2-average-point-without-magic-number

--- src/main/kotlin/lesson1/Less1_Test5.kt
+++ src/main/kotlin/lesson1/Less1_Test5.kt
@@ -2,8 +2,9 @@ package org.example.lesson1
 
 fun main(){
     val secondsOfJourney: Short = 6480
-    val minutesOfJourney = secondsOfJourney / 60
-    val hoursOfJourney = minutesOfJourney / 60
+    val period = 60
+    val minutesOfJourney = secondsOfJourney / period
+    val hoursOfJourney = minutesOfJourney / period
     println("${String.format("%02d", hoursOfJourney)}:${String.format("%02d", minutesOfJourney % 60)}:" +
             String.format("%02d", secondsOfJourney % 60)
     )
