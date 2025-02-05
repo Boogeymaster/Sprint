85228d3944283b6df338ed08a55d982b44614378
Re <qwe@mail.com>
2025-02-05 15:55:04 +0300

    KS-3-1 fixed punctuation

--- src/main/kotlin/lesson3/Less3_Test1.kt
+++ src/main/kotlin/lesson3/Less3_Test1.kt
@@ -3,9 +3,9 @@ package org.example.lesson3
 fun main() {
 
     val name = "Ivan"
-    val dayTime = listOf("Good day,", "Good evening,")
+    val dayTime = listOf("Good day", "Good evening")
     dayTime.forEach {
-        println("$it $name!")
+        println("$it, $name!")
     }
 
 }
