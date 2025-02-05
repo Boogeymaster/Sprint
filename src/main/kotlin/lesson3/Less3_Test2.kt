0624faf1a6a4da0aed86713513abea27c0c387d3
Re <qwe@mail.com>
2025-02-05 16:04:39 +0300

    KS-3-2 fixed

--- src/main/kotlin/lesson3/Less3_Test2.kt
+++ src/main/kotlin/lesson3/Less3_Test2.kt
@@ -3,11 +3,12 @@ package org.example.lesson3
 fun main() {
 
     val name = "Татьяна"
-    val surName = "Андреева"
+    var surName = "Андреева"
     val patronymic = "Сергеевна"
-    val marriedName = "Сидорова"
-    val age1 = 20
-    val age2 = 22
-    println("$surName $name $patronymic, $age1 лет")
-    println("$marriedName $name $patronymic, $age2 года")
+    var age = 20
+    println("$surName $name $patronymic, $age лет")
+    surName = "Сидорова"
+    age = 22
+    println("$surName $name $patronymic, $age лет")
+
 }
