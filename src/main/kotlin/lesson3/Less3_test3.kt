0825bb6fb771b9ed01b6043ac5139d7211c58664
Re <qwe@mail.com>
2025-02-06 12:47:20 +0300

    KS-3-3 without cycle

--- src/main/kotlin/lesson3/Less3_test3.kt
+++ src/main/kotlin/lesson3/Less3_test3.kt
@@ -3,7 +3,16 @@ package org.example.lesson3
 fun main() {
 
     val number = readln().toInt()
-    var table = ""
-    for (x in 1..9) table += "$number x $x = ${number * x}\n"
-    println(table)
+    println("""
+        $number x 1 = ${number * 1}
+        $number x 2 = ${number * 2}
+        $number x 3 = ${number * 3}
+        $number x 4 = ${number * 4}
+        $number x 5 = ${number * 5}
+        $number x 6 = ${number * 6}
+        $number x 7 = ${number * 7}
+        $number x 8 = ${number * 8}
+        $number x 9 = ${number * 9}
+    """.trimIndent())
+
 }
