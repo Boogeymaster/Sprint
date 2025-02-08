478a10964e4608099f466174af019cf49105817e
Re <qwe@mail.com>
2025-02-08 14:23:05 +0300

    KS-4-5 without magic

--- src/main/kotlin/lesson4/Less4_test5.kt
+++ src/main/kotlin/lesson4/Less4_test5.kt
@@ -1,17 +1,21 @@
 package org.example.lesson4
 
+const val MIN_CREW_COUNT = 55
+const val MAX_CREW_COUNT = 70
+const val MIN_PROVISION = 50
+
 fun main() {
     println("Наличие повреждений корпуса?")
-    val damage = readln().toBoolean()
+    val isDamage = readln().toBoolean()
     println("Текущий состав экипажа:")
     val crew = readln().toInt()
     println("Количество ящиков с провизией на борту:")
     val provision = readln().toInt()
     println("Благоприятность метеоусловий?")
-    val weather = readln().toBoolean()
+    val isWeather = readln().toBoolean()
     println("Может ли научно-исследовательский корабль приступить к долгосрочному плаванию?\nОтвет: " +
-            "${(!damage && crew in 55..70 && provision > 50) 
-                    || (damage && crew == 70 && weather && provision >= 50)}")
+            "${(!isDamage && crew >= MIN_CREW_COUNT && crew <= MAX_CREW_COUNT && provision > MIN_PROVISION) 
+                    || (isDamage && crew == MAX_CREW_COUNT && isWeather && provision >= MIN_PROVISION)}")
 
 }
 
