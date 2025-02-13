36f641e24afd899a9ffa12a3083ae5487e17b573
Re <qwe@mail.com>
2025-02-13 21:04:34 +0300

    KS-8-3 eleganse-search

--- src/main/kotlin/lesson8/Less8_test3.kt
+++ src/main/kotlin/lesson8/Less8_test3.kt
@@ -5,7 +5,7 @@ fun main() {
     val recipe = arrayOf("мясо", "лук", "картошка", "тесто")
     println("Введите искомый ингрeдиент:")
     val ingredient = readln()
-    if (recipe.indexOf(ingredient) > 0) {
+    if (ingredient in recipe) {
         println("Ингредиент $ingredient в рецепте есть")
     } else {
         println("Такого ингредиента в рецепте нет")
