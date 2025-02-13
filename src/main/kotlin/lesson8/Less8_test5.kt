dc4e14bb8c7fdea41f6d3dd8942dcd58cf49f4bb
Re <qwe@mail.com>
2025-02-13 22:09:22 +0300

    KS-8-5 lambda-array

--- src/main/kotlin/lesson8/Less8_test5.kt
+++ src/main/kotlin/lesson8/Less8_test5.kt
@@ -2,14 +2,8 @@ package org.example.lesson8
 
 fun main() {
 
-    println("Введите количество ингредиентов:")
-    val ingredients = Array(readln().toInt()) { "" }
-    println("Введите желаемые ингредиенты:")
-    ingredients.forEach {
-        ingredients[ingredients.indexOf(it)] = readln()
-    }
-    ingredients.forEach {
-        if (ingredients.indexOf(it) == ingredients.size - 1) print(it)
-        else print("$it, ")
-    }
+    println("Введите количество и сами ингредиенты по порядку:")
+    val ingredients = Array(readln().toInt()) { readln() }
+    println(ingredients.joinToString(", "))
+
 }
