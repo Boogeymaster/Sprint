eff8274351ed0ccaaf6ce4bf8c408421a4437a64
Re <qwe@mail.com>
2025-02-19 20:19:06 +0300

    KS-12-1-understand-task

--- src/main/kotlin/lesson12/Less12_test1.kt
+++ src/main/kotlin/lesson12/Less12_test1.kt
@@ -1,33 +1,33 @@
 package org.example.lesson12
 
 
-class Weather (_dayTemp: Int, _nightTemp: Int, _isRain: Boolean){
-    val dayTemp = _dayTemp
-    val nightTemp = _nightTemp
-    val isRain = _isRain
-    var date = "25.06.2025"
-    var dayOfWeek = "wednesday"
-    fun printWeather(){
-        println("""
+class Weather() {
+    var dayTemp = 25
+    var nightTemp = 25
+    var isRain = true
+    fun printWeather() {
+        println(
+            """
             Данные за день: 
             Температура днем: $dayTemp
             Температура ночью: $nightTemp
             Осадки: $isRain
-        """.trimIndent())
+        """.trimIndent()
+        )
     }
 }
 
 fun main() {
-
-    val day1 = Weather (25, 20, true)
-    val day2 = Weather (23, 20, false)
+    val day1 = Weather()
+    day1.dayTemp = 28
+    day1.nightTemp = 24
+    day1.isRain = true
     day1.printWeather()
+    val day2 = Weather()
+    day2.dayTemp = 31
+    day2.nightTemp = 28
+    day2.isRain = false
     day2.printWeather()
-    day1.date = "27.06.2025"
-    day1.dayOfWeek = "friday"
-    day2.date = "28.06.2025"
-    day2.dayOfWeek = "saturday"
-
 }
 
 
