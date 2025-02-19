be6fa979cd2d9c8f992e17d19d6a55e721067ea2
Re <qwe@mail.com>
2025-02-19 20:01:35 +0300

    KS-12-2

---
+++ src/main/kotlin/lesson12/Less12_test2.kt
@@ -0,0 +1,24 @@
+package org.example.lesson12
+
+class Weather2 ( val dayTemp: Int, val nightTemp: Int, val isRain: Boolean){
+
+    var date = "25.06.2025"
+    var dayOfWeek = "wednesday"
+    fun printWeather(){
+        println("""
+            Данные за день: 
+            Температура днем: $dayTemp
+            Температура ночью: $nightTemp
+            Осадки: $isRain
+        """.trimIndent())
+    }
+}
+
+fun main() {
+
+    val day1 = Weather2(32, 28, false)
+    day1.printWeather()
+
+}
+
+
