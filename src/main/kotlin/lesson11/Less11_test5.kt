df99f39472bbb63144b8adec33b7a3c93eaece81
Re <qwe@mail.com>
2025-02-19 20:45:09 +0300

    KS-11-5 remove-excess

--- src/main/kotlin/lesson11/Less11_test5.kt
+++ src/main/kotlin/lesson11/Less11_test5.kt
@@ -4,18 +4,16 @@ class Forum(
     val members: MutableList<MemberOfForum> = mutableListOf(),
     val messages: MutableList<MessageOfForum> = mutableListOf(),
 ) {
-    fun createNewUser(name: String, forum: Forum): Forum {
-        forum.members.add(MemberOfForum(forum.members.size, name))
-        return forum
+    fun createNewUser(name: String) {
+        members.add(MemberOfForum(members.size, name))
     }
 
-    fun createNewMessage(id: Int, forum: Forum): Forum {
+    fun createNewMessage(id: Int){
         var isMember = false
-        forum.members.forEach {
+        members.forEach {
             if (id == it.userId) isMember = true
         }
-        if (isMember) forum.messages.add(MessageOfForum(id, "Это автоматическое сообщение."))
-        return forum
+        if (isMember) messages.add(MessageOfForum(id, "Это автоматическое сообщение."))
     }
 
     fun printThread(forum: Forum) {
@@ -35,45 +33,15 @@ class MessageOfForum(
 
 
 fun main() {
-
     val forum = Forum()
-    forum.createNewUser("Строитель №1", forum)
-    forum.createNewUser("Строитель №2", forum)
-    forum.createNewMessage(forum.members[0].userId, forum)
-    forum.createNewMessage(forum.members[0].userId, forum)
-    forum.createNewMessage(forum.members[1].userId, forum)
-    forum.createNewMessage(forum.members[1].userId, forum)
+    forum.createNewUser("Строитель №1")
+    forum.createNewUser("Строитель №2")
+    forum.createNewMessage(forum.members[0].userId)
+    forum.createNewMessage(forum.members[0].userId)
+    forum.createNewMessage(forum.members[1].userId)
+    forum.createNewMessage(forum.members[1].userId)
     forum.printThread(forum)
 }
-
-
-//Задача 5* к Уроку 11
-//
-//Реализовать класс работы форума (Forum).
-//Для реализации создай дополнительные классы для сущностей
-//“Член форума” (с полями userId и userName)
-//и “Сообщение форума” (с полями authorId и message).
-//
-//Примени паттерн “Строитель” для создания
-//пользователей и сообщений форума. Напрямую
-//объекты не создаются, логика должна быть
-//инкапсулирована внутри методов класса.
-//
-//- createNewUser() создает новых пользователей,
-//принимая имя пользователя. Метод сохраняет нового
-//пользователя в общий список и одновременно возвращает новый объект.
-//Генерация id-шников новых пользователей происходит внутри;
-//- createNewMessage() создает сообщения, принимая
-//id пользователя. Сообщения создаются только если
-//такой пользователь есть на форуме;
-//- printThread() печатает в консоль все сообщения
-//добавленные на форум в формате:
-//
-//автор: сообщение
-//автор: сообщение
-//
-//Создай экземпляр Forum и продемонстрируй
-//его работу, добавив двух пользователей и по два сообщения от каждого пользователя.
 
 
 
