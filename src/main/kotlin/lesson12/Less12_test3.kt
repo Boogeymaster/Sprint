cdb3c1d7db0db81f7628e0668824e409d975d4c5
Re <qwe@mail.com>
2025-02-19 20:59:52 +0300

    KS-12-3 fix-formatting

--- src/main/kotlin/lesson12/Less12_test3.kt
+++ src/main/kotlin/lesson12/Less12_test3.kt
@@ -1,21 +1,24 @@
 package org.example.lesson12
 
 
-class Weather3 (dayTemp: Int, nightTemp: Int, private val isRain: Boolean){
+class Weather3(dayTemp: Int, nightTemp: Int, private val isRain: Boolean) {
     private val dayTemp = dayTemp - 273
     private val nightTemp = nightTemp - 273
-    fun printWeather(){
-        println("""
+
+    fun printWeather() {
+        println(
+            """
             Данные за день: 
             Температура днем: $dayTemp °С
             Температура ночью: $nightTemp °С
             Осадки: $isRain 
-        """.trimIndent())
+        """.trimIndent()
+        )
     }
 }
 
 fun main() {
-    val day = Weather3 (290,288, false)
+    val day = Weather3(290, 288, false)
     day.printWeather()
 }
 
