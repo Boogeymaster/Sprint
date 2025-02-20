b75c0a77fe961d442564bdec5ad85b251177a9b5
Re <qwe@mail.com>
2025-02-20 20:49:17 +0300

    KS-12-5

--- src/main/kotlin/lesson12/Less12_test5.kt
+++ src/main/kotlin/lesson12/Less12_test5.kt
@@ -19,17 +19,45 @@ class Weather4(_dayTemp: Int, _nightTemp: Int, private val isRain: Boolean) {
         """.trimIndent()
         )
     }
+
+    fun getDayTemp(): Int {
+        return dayTemp
+    }
+
+    fun getNightTemp(): Int {
+        return nightTemp
+    }
+
+    fun getIsRain(): Boolean {
+        return isRain
+    }
 }
 
 fun main() {
-    val tempRange = 15..35
+    val dayTempRange = 288..308
+    val nightTempRange = 283..303
     val random = Random
     val month: MutableList<Weather4> = mutableListOf()
     for (d in 1..DAY_IN_MONTH) {
-        month.add(Weather4(tempRange.random(), tempRange.random(), random.nextBoolean()))
+        month.add(Weather4(dayTempRange.random(), nightTempRange.random(), random.nextBoolean()))
     }
-
+    val dayTempList = month.map { it.getDayTemp() }
+    val nightTempList = month.map { it.getNightTemp() }
+    val isRainList = month.map { it.getIsRain() }
+    val averageDayTemp = dayTempList.average()
+    val averageNightTemp = nightTempList.average()
+    var isRainSum = 0
+    for (i in isRainList) {
+        if (i) isRainSum++
+    }
+    println("""
+        Средняя дневная температура за месяц: ${"%.2f".format(averageDayTemp)} 
+        Средняя ночная температура за месяц: ${"%.2f".format(averageNightTemp)}    
+        Количсетво дождливых дней: $isRainSum
+    """.trimIndent())
 }
+
+
 
 //Задача 5* к Уроку 12
 //
