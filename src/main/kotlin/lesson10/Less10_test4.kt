3b3c603aa1e7406fa0fe44f733b1597b2c73ed81
Re <qwe@mail.com>
2025-02-20 16:46:33 +0300

    KS-10-4 i-forget

--- src/main/kotlin/lesson10/Less10_test4.kt
+++ src/main/kotlin/lesson10/Less10_test4.kt
@@ -6,14 +6,14 @@ fun main() {
 
     var userWinCounter = 0
     do {
-        val game = arrayOf(randomNumber(), randomNumber())
+        val game = arrayOf(randomNumber2(), randomNumber2())
         if (resultOfGame(game)) userWinCounter++
         println("Хотите бросить кости еще раз? Введите Да или Нет")
     } while (readln().equals(YES, true))
     println("Количество побед человека : $userWinCounter")
 }
 
-fun randomNumber(): Int = (1..6).random()
+fun randomNumber2(): Int = (1..6).random()
 
 fun resultOfGame(game: Array<Int>): Boolean {
     return if (game[0] > game[1]) true
