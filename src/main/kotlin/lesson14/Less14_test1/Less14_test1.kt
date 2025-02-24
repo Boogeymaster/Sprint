be06662169333a7bf12afd1ff636393eb68ba4b0
Re <qwe@mail.com>
2025-02-24 08:34:28 +0300

    KS-14-1 unification-classes

--- src/main/kotlin/lesson14/Less14_test1/Less14_test1.kt
+++ src/main/kotlin/lesson14/Less14_test1/Less14_test1.kt
@@ -1,5 +1,34 @@
 package org.example.lesson14.Less14_test1
 
+open class Liner(
+    val name: String,
+    val speed: Int,
+    val passengers: Int,
+)
+
+class IceBreaker(
+    name: String,
+    speed: Int,
+    passengers: Int,
+    cargoVolume: Int,
+    isIceBreak: Boolean,
+) : Liner(
+    name,
+    speed,
+    passengers,
+)
+
+class CargoShip(
+    name: String,
+    speed: Int,
+    passengers: Int,
+    cargoVolume: Int,
+) : Liner(
+    name,
+    speed,
+    passengers,
+)
+
 fun main() {
 
     val ship1 = Liner("Titanic", 32, 2000)
--- src/main/kotlin/lesson14/Less14_test1/Liner.kt
+++
@@ -1,8 +0,0 @@
-package org.example.lesson14.Less14_test1
-
-open class Liner(
-    val name: String,
-    val speed: Int,
-    val passengers: Int,
-) {
-}
