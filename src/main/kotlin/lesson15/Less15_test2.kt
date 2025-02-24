fd44bae419aa9bb0e15831e826d23cb635029ed1
Re <qwe@mail.com>
2025-02-24 19:40:25 +0300

    KS-15-2

--- src/main/kotlin/lesson15/Less15_test2.kt
+++ src/main/kotlin/lesson15/Less15_test2.kt
@@ -1,40 +1,22 @@
 package org.example.lesson15
 
-abstract class WeatherStationStats{
-    open fun sendStats(){}
+abstract class WeatherStationStats() {
+    abstract val weatherData: Int
 }
 
-class Temperature(): WeatherStationStats(){
-    override fun sendStats() {
-        println("Отправка темепературы")
-    }
-}
-class PrecipitationAmount(): WeatherStationStats(){
-    override fun sendStats() {
-        println("Отправка количества осадков")
-    }
-}
-class WeatherServer(weatherStats: WeatherStationStats){
+class Temperature(override val weatherData: Int) : WeatherStationStats()
 
+class PrecipitationAmount(override val weatherData: Int) : WeatherStationStats()
+
+class WeatherServer(weatherStats: WeatherStationStats) {
+    init {
+        if (weatherStats is Temperature) println("${weatherStats.weatherData} градусов")
+        if (weatherStats is PrecipitationAmount) println("${weatherStats.weatherData} мм")
+    }
 }
 
 fun main() {
-
-
-
+    val weatherServer1 = WeatherServer(Temperature(35))
+    val weatherServer2 = WeatherServer(PrecipitationAmount(100))
 }
 
-//Задача 2 к Уроку 15
-//
-//Необходимо разработать простую систему мониторинга погодных условий,
-//которая будет собирать данные о температуре и количестве осадков.
-//
-//Создай абстрактный класс WeatherStationStats.
-//Он будет служить родителем для классов-объектов погодных данных:
-//Temperature и PrecipitationAmount.
-//
-//Реализуй логику имитации отправки сообщения на сервер в классе WeatherServer.
-//Метод отправки сообщения должен принимать объект типа
-//WeatherStationStats и, в зависимости от типа переданного объекта, выводить соответствующие данные.
-//
-//Создай по одному объекту для разных типов погодных данных и протестируй работу программы.
