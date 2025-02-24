a3c9c8090e3ec3850bbe242a2dc6f96f6d6b508c
Re <qwe@mail.com>
2025-02-24 08:36:59 +0300

    KS-14-2 unification-classes

--- src/main/kotlin/lesson14/Less14_test2/Less14_test2.kt
+++ src/main/kotlin/lesson14/Less14_test2/Less14_test2.kt
@@ -1,5 +1,69 @@
 package org.example.lesson14.Less14_test2
 
+open class Liner1(
+    val name: String,
+    val speed: Int,
+    val passengers: Int,
+) {
+    open fun loading() {
+        println("горизонтальный трап со шкафута")
+    }
+
+    open fun printData() {
+        println(
+            """
+            
+            Название корабля: $name
+            скорость: $speed узлов
+            Количество пассажиров: $passengers
+            Погрузка через:
+        """.trimIndent()
+        )
+        loading()
+    }
+}
+
+class IceBreaker1(
+    name: String,
+    speed: Int,
+    passengers: Int,
+    val cargoVolume: Int,
+    val isIceBreak: Boolean,
+) : Liner1(
+    name,
+    speed,
+    passengers,
+) {
+    override fun loading() {
+        println("ворота со стороны кормы")
+    }
+
+    override fun printData() {
+        super.printData()
+        println("Объем трюма: $cargoVolume м³\nВозможность пролома льдов: $isIceBreak")
+    }
+}
+
+class CargoShip1(
+    name: String,
+    speed: Int,
+    passengers: Int,
+    val cargoVolume: Int,
+) : Liner1(
+    name,
+    speed,
+    passengers,
+) {
+    override fun loading() {
+        println("погрузочный кран")
+    }
+
+    override fun printData() {
+        super.printData()
+        println("Объем трюма: $cargoVolume м³")
+    }
+}
+
 fun main() {
     val ship1 = Liner1("Titanic", 32, 2000)
     val ship2 = CargoShip1("Ever Green", 20, 30, 500000)
