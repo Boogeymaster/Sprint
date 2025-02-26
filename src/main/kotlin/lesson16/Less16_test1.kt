7148798bc477ffb89757e428cf65e94b4cf02015
Re <qwe@mail.com>
2025-02-26 18:32:17 +0300

    KS-16-1

---
+++ src/main/kotlin/lesson16/Less16_test1.kt
@@ -0,0 +1,14 @@
+package org.example.lesson16
+
+class Cube {
+    private val number = 5
+
+    fun outputNumber() {
+        println(number)
+    }
+}
+
+fun main() {
+    val cube = Cube()
+    cube.outputNumber()
+}
