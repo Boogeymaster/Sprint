d43435318248fb40aee34a908d3e01c09975eeab
Re <qwe@mail.com>
2025-02-26 19:25:08 +0300

    KS-16-4

---
+++ src/main/kotlin/lesson16/Less16_test4.kt
@@ -0,0 +1,23 @@
+package org.example.lesson16
+
+class Order() {
+    private val orderNumber = 101
+    private var isPrepare = false
+
+    fun request(isPrepare: Boolean){
+        if(isPrepare) changeStatus()
+    }
+
+    private fun changeStatus() {
+        isPrepare = true
+        println("Order is prepare")
+    }
+
+}
+
+fun main() {
+
+    val order = Order()
+    order.request(true)
+
+}
