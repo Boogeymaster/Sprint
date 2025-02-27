b6f3ce4c22e5bd1e681774bc7daba1930f2b2bd7
Re <qwe@mail.com>
2025-02-27 18:31:32 +0300

    KS-16-2 fix-functions

--- src/main/kotlin/lesson16/Less16_test2.kt
+++ src/main/kotlin/lesson16/Less16_test2.kt
@@ -5,19 +5,24 @@ const val PERIMETER_COEFF = 2
 class Round(_radius: Int) {
     private val pi = 3.14
     private val radius = _radius
-    fun perimeter() {
+    fun perimeter(): Double {
         val perimeter = PERIMETER_COEFF * pi * radius
-        println("Длина окружности: ${"%.2f".format(perimeter)}")
+        return perimeter
     }
 
-    fun area() {
+    fun area(): Double {
         val area = pi * radius * radius
-        println("Площадь круга: $area")
+        return area
     }
+
+    fun printData() {
+        println("Длина окружности: ${"%.2f".format(perimeter())}")
+        println("Площадь круга: ${area()}")
+    }
+
 }
 
 fun main() {
     val round = Round(5)
-    round.perimeter()
-    round.area()
+    round.printData()
 }
