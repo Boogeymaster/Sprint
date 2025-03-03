52806dd2636224569641b957b16344891d129a07
Re <qwe@mail.com>
2025-03-03 13:18:53 +0300

    KS-9-5

--- src/main/kotlin/lesson9/Les9_test5.kt
+++ src/main/kotlin/lesson9/Les9_test5.kt
@@ -7,9 +7,10 @@ fun main() {
 
     println("Введите пять названий ингредиентов: ")
     val ingredients = MutableList(INGREDIENTS_COUNT) { readln() }
-    val ingredientsSet = ingredients.distinct()
-    val ingredientsSortedSet = ingredientsSet.sorted().toMutableList()
-    ingredientsSortedSet[0] = ingredientsSortedSet[0].replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
-    println(ingredientsSortedSet.joinToString(", "))
+    val ingredientsSet = ingredients.toSortedSet()
+    val firstIngredient = ingredientsSet.first.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+    ingredientsSet.remove(ingredientsSet.first())
+    ingredientsSet.add(firstIngredient)
+    println(ingredientsSet.joinToString(", "))
 
 }
