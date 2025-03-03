8057a1495182e2a00343c1a0e8b4ed355ffd8d19
Re <qwe@mail.com>
2025-03-03 14:16:54 +0300

    KS-18-2 realize-with-range

--- src/main/kotlin/lesson_18/Less18_test2.kt
+++ src/main/kotlin/lesson_18/Less18_test2.kt
@@ -1,38 +1,40 @@
 package org.example.lesson_18
 
-open class Dice {
-    open fun printNumber() = ""
+const val RANGE_START = 1
+
+abstract class Dice {
+    open fun printNumber() = 0
 }
 
-class Dice4 : Dice() {
-    private val edges = arrayOf("1", "2", "3", "4")
+class Dice4(edges: Int) : Dice() {
+    private val numberRange = (RANGE_START..edges)
 
-    override fun printNumber(): String {
-        return edges[edges.indices.random()]
+    override fun printNumber(): Int {
+        return numberRange.random()
     }
 }
 
-class Dice6 : Dice() {
-    private val edges = arrayOf("1", "2", "3", "4", "5", "6")
+class Dice6(edges: Int) : Dice() {
+    private val numberRange = (RANGE_START..edges)
 
-    override fun printNumber(): String {
-        return edges[edges.indices.random()]
+    override fun printNumber(): Int {
+        return numberRange.random()
     }
 }
 
-class Dice8 : Dice() {
-    private val edges = arrayOf("1", "2", "3", "4", "5", "6", "7", "8")
+class Dice8(edges: Int) : Dice() {
+    private val numberRange = (RANGE_START..edges)
 
-    override fun printNumber(): String {
-        return edges[edges.indices.random()]
+    override fun printNumber(): Int {
+        return numberRange.random()
     }
 }
 
 fun main() {
 
-    val dice4 = Dice4()
-    val dice6 = Dice6()
-    val dice8 = Dice8()
+    val dice4 = Dice4(4)
+    val dice6 = Dice6(6)
+    val dice8 = Dice8(8)
     val diceList: List<Dice> = listOf(dice4, dice6, dice8)
     diceList.forEach {
         println(it.printNumber())
