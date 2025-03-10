0e062f885abc9f61cbf8f2131cffa33abaf7f838
Re <qwe@mail.com>
2025-03-10 09:00:20 +0300

    KS-19-5 extend-enum-class

--- src/main/kotlin/lesson19/Less19_test5.kt
+++ src/main/kotlin/lesson19/Less19_test5.kt
@@ -4,16 +4,27 @@ const val LIST_LENGTH = 5
 
 class Man(val name: String, val gender: Gender)
 
-enum class Gender() {
+enum class Gender {
     MEN,
     WOMEN,
+    ERROR,
+    UNDEFINDED,
 }
 
 fun main() {
-    println("Please input list of 5 people with name and gender\nAvailable next gender: ${Gender.entries}")
-    val peopleList: Array<Man> = Array(LIST_LENGTH) { Man(readln(), Gender.valueOf(readln())) }
+    println("Please input list of 5 people with name and gender\nAvailable next gender: ${Gender.MEN} and ${Gender.WOMEN}")
+    val peopleList: Array<Man> = Array(LIST_LENGTH) { Man(readln(), selectGender(readln())) }
     peopleList.forEach {
-        println("${it.name} ${it.gender.name}")
+        println("Name: ${it.name}, gender: ${it.gender}")
     }
 
+}
+
+fun selectGender(input: String): Gender {
+    return when (input) {
+        "MEN" -> Gender.MEN
+        "WOMEN" -> Gender.WOMEN
+        "" -> Gender.ERROR
+        else -> Gender.UNDEFINDED
+    }
 }
