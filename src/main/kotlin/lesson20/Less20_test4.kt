2eae2272dfd36b549910460ec52939088c7e7159
Re <qwe@mail.com>
2025-03-16 13:41:34 +0300

    KS-20-4 fix-list

--- src/main/kotlin/lesson20/Less20_test4.kt
+++ src/main/kotlin/lesson20/Less20_test4.kt
@@ -3,7 +3,7 @@ package org.example.lesson20
 
 fun main() {
 
-    val elementList = listOf("01", "02", "01", "02", "01", "02", "01", "02")
+    val elementList = listOf("01", "02", "03", "04", "05", "06", "07", "08")
     val mappedElements = elementList.map { "Нажат элемент $it" }
     for (i in 1..<mappedElements.size step 2) {
         println(mappedElements[i])
