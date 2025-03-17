3f0c2f6662b5d56e2add9b158c09734a4574eae2
Re <qwe@mail.com>
2025-03-17 20:26:49 +0300

    KS-21-5

--- src/main/kotlin/lesson21/Less21_test5.kt
+++ src/main/kotlin/lesson21/Less21_test5.kt
@@ -3,8 +3,6 @@ package org.example.lesson21
 import kotlin.math.max
 
 fun Map<String, Int>.maxCategory(): String {
-    val maxValue = this.maxOf { it.value }
-    val categoryNames = this.filterValues { it == maxValue}.keys
-    return categoryNames.first()
+    val maxValue = this.maxByOrNull { it.value }
+    return maxValue?.key ?: ""
 }
-
